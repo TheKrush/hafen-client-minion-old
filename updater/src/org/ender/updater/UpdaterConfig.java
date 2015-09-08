cf5d2b738d1a65c42452c623feb2b742e18509fe
Keith Yates <krush@lostminions.org>
2015-09-07 23:56:10 -0700

    cleaning up imports

--- updater/src/org/ender/updater/UpdaterConfig.java
+++ updater/src/org/ender/updater/UpdaterConfig.java
@@ -1,20 +1,16 @@
 package org.ender.updater;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class UpdaterConfig {
 
