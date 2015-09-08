10a1be9276585133061bb372160463075047fe23
Keith <krush@lostminions.org>
2015-09-08 12:50:53 -0700

    hafen-client-minion -> hafen, updating run-test.bat

--- run-test.bat
+++ run-test.bat
@@ -1,9 +1,14 @@
-xcopy ".\dist\hafen.jar" ".\updater\dist\hafen.jar" /R /Y
-
-cd ".\updater\dist\"
-
 @echo off
-
+echo Copying Files ...
+echo f | xcopy ".\hafen\dist\hafen.jar" ".\hafen-minion\dist\hafen.jar" /R /Y
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+	goto :eof
+)
+cd ".\hafen-minion\dist\"
+echo Running hafen-minion.jar ...
 java -Xms512m -Xmx1024m -jar hafen-minion.jar TESTING
-
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+)
 cd ".\..\..\"
