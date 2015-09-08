80926c7dce82571d754c4b4f0cd55a246edbc9e6
Ender Wiggin <andrii.gook@gmail.com>
2015-09-08 21:18:26 +0300

    show error GUI even if error handler URL is not set

--- src/haven/error/ErrorHandler.java
+++ src/haven/error/ErrorHandler.java
@@ -94,6 +94,10 @@ public class ErrorHandler extends ThreadGroup {
 	private void doreport(Report r) throws IOException {
 	    if(!status.goterror(r.t))
 		return;
+	    if( errordest == null){
+		status.done(null, null);
+		return;
+	    }
 	    URLConnection c = errordest.openConnection();
 	    status.connecting();
 	    c.setDoOutput(true);
