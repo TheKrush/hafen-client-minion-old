56894ad32afa13a3f4f64953fb035e06a1bf4b5a
Ender Wiggin <andrii.gook@gmail.com>
2015-09-09 12:36:27 +0300

    small fix to how dumped maps draw ridges to make sure they do not depend on surrounding tiles

--- src/haven/MapDumper.java
+++ src/haven/MapDumper.java
@@ -130,8 +130,8 @@ public class MapDumper implements Defer.Callable<Object> {
 		}
 	    }
 	}
-	for(c.y = 0; c.y < sz.y; c.y++) {
-	    for(c.x = 0; c.x < sz.x; c.x++) {
+	for(c.y = 1; c.y < sz.y - 1; c.y++) {
+	    for(c.x = 1; c.x < sz.x - 1; c.x++) {
 		try {
 		    int t = grid.gettile(c);
 		    Coord c0 = c.add(grid.ul);
