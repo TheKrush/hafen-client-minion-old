4181026321be2baab5a326c1eaf6583a0630f933
Ender Wiggin <andrii.gook@gmail.com>
2015-09-10 15:05:30 +0300

    fixed keydown in textEntry

--- src/haven/TextEntry.java
+++ src/haven/TextEntry.java
@@ -161,7 +161,8 @@ public class TextEntry extends SIWidget {
     }
 
     public boolean keydown(KeyEvent e) {
-	return(buf.key(e));
+	buf.key(e);
+	return true;
     }
 
     public boolean mousedown(Coord c, int button) {
