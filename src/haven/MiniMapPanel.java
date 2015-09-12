0ce3c2b9f89bca12dacadb96cea9e009389f882a
Ender Wiggin <andrii.gook@gmail.com>
2015-09-12 21:32:51 +0300

    added buttons for showing personal/village claim to minimap panel

--- src/haven/MiniMapPanel.java
+++ src/haven/MiniMapPanel.java
@@ -7,6 +7,28 @@ public class MiniMapPanel extends ResizingWindow {
     public MiniMapPanel(Coord sz) {
 	super(sz, "Minimap");
 	justclose = true;
+	addtwdg(add(new IButton("gfx/hud/mmap/claim", "", "-d", "-h"){
+	    {tooltip = Text.render("Display personal claims");}
+	    public void click() {
+		if(ui != null && ui.gui != null && ui.gui.map != null) {
+		    if(!ui.gui.map.visol(0))
+			ui.gui.map.enol(0, 1);
+		    else
+			ui.gui.map.disol(0, 1);
+		}
+	    }
+	}));
+	addtwdg(add(new IButton("gfx/hud/mmap/vil", "", "-d", "-h"){
+	    {tooltip = Text.render("Display village claims");}
+	    public void click() {
+		if(ui != null && ui.gui != null && ui.gui.map != null) {
+		    if(!ui.gui.map.visol(2))
+			ui.gui.map.enol(2, 3);
+		    else
+			ui.gui.map.disol(2, 3);
+		}
+	    }
+	}));
     }
 
     @Override
