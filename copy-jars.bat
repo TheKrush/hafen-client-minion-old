4ff23fae744defe56036c46781cbef144b43f61e
TheKrush <krush@lostminions.org>
2015-09-14 19:20:56 -0700

    copy files to a different location

--- copy-jars.bat
+++ copy-jars.bat
@@ -1,8 +1,9 @@
 @echo off
 echo Copying Files ...
-xcopy ".\hafen\dist\hafen.jar" ".\files\hafen.jar" /R /Y
-xcopy ".\hafen-minion\dist\hafen-minion.jar" ".\files\hafen-minion.jar" /R /Y
+echo f|xcopy ".\hafen\dist\hafen.jar" "..\TheKrush.github.io\files\hafen-client-minion\hafen.jar" /R /Y
+echo f|xcopy ".\hafen-minion\dist\hafen-minion.jar" "..\TheKrush.github.io\files\hafen-client-minion\hafen-minion.jar" /R /Y
 echo Removing Project Folders ...
 rmdir ".\hafen\" /S /Q
+rmdir ".\hafen-layer-util\" /S /Q
 rmdir ".\hafen-map-combiner\" /S /Q
 rmdir ".\hafen-minion\" /S /Q
